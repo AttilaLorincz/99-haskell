040edfb1d1210bf52b7ec38f1d052850e5cdf42f
AttilaLorincz <attila.lorincz+github@gmail.com>
2015-01-01 10:52:39 +0100

    Solutions to problems 11-16

--- 99-problems/src/Problems-11-20.hs
+++ 99-problems/src/Problems-11-20.hs
@@ -35,9 +35,22 @@ tests_11 =
   ]
 
 encodeModified :: (Eq a) => [a] -> [Run a]
-encodeModified = undefined
+encodeModified xs = [tupletorun e | e <- encode xs] where 
+	tupletorun (1,x) = Single x 
+	tupletorun (n,x) = Multiple n x
+	encode :: (Eq a) => [a] -> [(Int, a)]
+	encode l = [(length i, head i) | i<- pack l ] where 
+		pack :: (Eq a) => [a] -> [[a]]
+		pack [] = []	
+		pack (x:xs) = reverse $ packl (x:xs) [] where
+			packl :: (Eq a) => [a] -> [[a]] -> [[a]]
+			packl [] acc = acc 
+			packl (x:xs) acc 
+					| (acc /= [] && x == head (head acc)) = packl xs $ (x : (head acc )) : (tail acc)
+					| otherwise =  packl xs $ [x] : acc 
 
-data Run a
+
+data Run a 
   = Single a
   | Multiple Int a
   deriving (Eq, Show)
@@ -54,7 +67,11 @@ tests_12 =
   ]
 
 decodeModified :: [Run a] -> [a]
-decodeModified = undefined
+decodeModified [] = []
+decodeModified (ra:ras) = runtolist ra ++ decodeModified ras where 
+	runtolist (Single x) = [x]
+--	runtolist (Multiple n x) = take n $ [x | _ <- [1,1..] ] 
+	runtolist (Multiple n x) = replicate n x 
 
 -- Problem 13: Run-length encoding of a list (direct solution).
 -- Don't explicitly create the sublists containing the
@@ -69,8 +86,15 @@ tests_13 =
   ]
 
 encodeDirect :: (Eq a) => [a] -> [Run a]
-encodeDirect = undefined
-
+encodeDirect = foldr encodeDirectCount [] where
+     encodeDirectCount x [] = [Single x]
+     encodeDirectCount x (h@(Single b):rs)
+			| x == b = (Multiple 2 x):rs
+			| otherwise = (Single x):h:rs
+     encodeDirectCount x (h@(Multiple n b):rs)
+			| x == b = (Multiple (n+1) x):rs
+			| otherwise = (Single x):h:rs
+			
 -- Problem 14: Duplicate the elements of a list.
 tests_14 :: [Property]
 tests_14 =
@@ -79,7 +103,7 @@ tests_14 =
   ]
 
 dupli :: [a] -> [a]
-dupli = undefined
+dupli = repli 2
 
 -- Problem 15: Replicate the elements of a list a given number of times.
 tests_15 :: [Property]
@@ -89,7 +113,7 @@ tests_15 =
   ]
 
 repli :: Int -> [a] -> [a]
-repli = undefined
+repli n loa = concat[replicate n i | i <- loa] 
 
 -- Problem 16: Drop every Nth element from a list.
 tests_16 :: [Property]
@@ -99,7 +123,7 @@ tests_16 =
   ]
 
 dropEvery :: Int -> [a] -> [a]
-dropEvery = undefined
+dropEvery n loa = [ i | (c, i) <- ( zip [1,2..] loa), (c `mod` n) /= 0]
 
 -- Problem 17: Split a list into two parts; the length of the first part is given.
 tests_17 :: [Property]
